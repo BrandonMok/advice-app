8c56b019db21741f17de1e9616f17d2933f6b746
BrandonMok <brandonmok88@yahoo.com>
2022-05-04 20:56:09 -0400

    initial commit

---
+++ src/components/banner.js
@@ -0,0 +1,10 @@
+
+export default function Banner() {
+    return (
+        <div className="banner">
+            <div className="banner__intro">
+                Looking for advice?
+            </div>
+        </div>
+    );
+}
---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header__intro">Advice Generator</div>
+        </div>
+    );
+}
