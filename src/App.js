f65d60ee41ec28f5f82c357a6856f30238a971f9
BrandonMok <brandonmok88@yahoo.com>
2022-05-06 08:47:59 -0400

    cleaned up banner styling / structure

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Header />
       <Banner />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Footer />
     </div>
   );
--- src/components/banner.js
+++ src/components/banner.js
@@ -1,44 +1,50 @@
 import React, { useEffect } from 'react';
-// import anime from 'animejs/lib/anime.es.js';
+import anime from 'animejs/lib/anime.es.js';
 
 export default function Banner() {
-    // useEffect(() => {
-    //     anime.timeline()
-    //     .add({
-    //       targets: '.banner__intro__text',
-    //       opacity: 0,
-    //       duration: 200,
-    //       easing: "easeOutExpo",
-    //     })
-    //     .add({
-    //         targets: '.banner__intro__text',
-    //         easing: "easeOutExpo",
-    //         rotateY: [-90, 0],
-    //         duration: 1300,
-    //         opacity: 1,
-    //         delay: 500
-    //     })
-    //     .add({
-    //         targets: '.banner__intro__subtext',
-    //         opacity: 0,
-    //         duration: 200,
-    //         easing: "easeOutExpo",
-    //       })
-    //       .add({
-    //           targets: '.banner__intro__subtext',
-    //           easing: "easeOutExpo",
-    //           rotateY: [-90, 0],
-    //           duration: 1300,
-    //           opacity: 1,
-    //           delay: 500
-    //       });
-    // }, []);
+    useEffect(() => {
+        anime.timeline()
+        .add({
+          targets: '.banner__intro__text',
+          opacity: 0,
+          duration: 200,
+          easing: "easeOutExpo",
+        })
+        .add({
+            targets: '.banner__intro__text',
+            easing: "easeOutExpo",
+            rotateY: [-90, 0],
+            duration: 1300,
+            opacity: 1,
+            delay: 500
+        })
+        .add({
+            targets: '.banner__intro__subtext',
+            opacity: 0,
+            duration: 200,
+            easing: "easeOutExpo",
+          })
+          .add({
+              targets: '.banner__intro__subtext',
+              easing: "easeOutExpo",
+              rotateY: [-90, 0],
+              duration: 1300,
+              opacity: 1,
+              delay: 500
+          });
+    }, []);
 
     return (
         <div className="banner">
-            <div className="banner__intro">
-                <div className="banner__intro__text">Looking for Advice?</div>
-                <div className='banner__intro__subtext'>Ask a way</div>
+            <div className="container banner__intro">
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className="banner__intro__text">Looking for Advice?</div>
+                    </div>
+                    <div className='col-12'>
+                        <div className='banner__intro__subtext'>Ask a way</div>
+                    </div>
+                </div>
             </div>
         </div>
     );
