5559c857833ea970f1cd331c5b4dabbf53025578
BrandonMok <brandonmok88@yahoo.com>
2022-05-06 18:30:31 -0400

    testimonials flipped order grid fix

--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -1,9 +1,12 @@
 import React from "react";
+import OpenQuote from '../icons/openQuote.svg';
+import CloseQuote from '../icons/closingQuote.svg';
+
 // import PropTypes from 'prop-types';
 
 export default function Testimonial({td}) {
     const orderLayoutModifier = td.order ? '' : 'testimonial--order2';
-    
+
     return (
         <div className={`testimonial ${orderLayoutModifier}`}>
             <div className="testimonial__container container">
@@ -14,17 +17,29 @@ export default function Testimonial({td}) {
                                 <img src={td.icon} alt='Avatar' />
                             </div>
                             <div className="col-12 col-md-6">
-                                <div className="testimonial__container__name">{td.name}</div>
-                                <div className="testimonial__container__desc">{td.desc}</div>
+                                <div className="testimonial__container__textContent">
+                                    <div className="testimonial__container__textContent__name">{td.name}</div>
+                                    <div className="testimonial__container__textContent__desc">
+                                        <img src={OpenQuote} alt='SVG open quote' />
+                                        {td.desc}
+                                        <img src={CloseQuote} alt='SVG close quote' />
+                                    </div>
+                                </div>
                             </div>
                         </>
                     ) : (
                         <>
-                            <div className="col-12 col-md-6">
-                                <div className="testimonial__container__name">{td.name}</div>
-                                <div className="testimonial__container__desc">{td.desc}</div>
+                            <div className="col-12 col-md-6 order-2 order-md-1">
+                                <div className="testimonial__container__textContent">
+                                    <div className="testimonial__container__textContent__name">{td.name}</div>
+                                    <div className="testimonial__container__textContent__desc">
+                                        <img src={OpenQuote} alt='SVG open quote' />
+                                        {td.desc}
+                                        <img src={CloseQuote} alt='SVG close quote' />
+                                    </div>
+                                </div>
                             </div>
-                            <div className="col-12 col-md-6">
+                            <div className="col-12 col-md-6 order-1 order-md-2">
                                 <img src={td.icon} alt='Avatar' />
                             </div>
                         </>
