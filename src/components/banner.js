9edfc66f87175d57b2a4466b8dd645cf39e331d9
BrandonMok <brandonmok88@yahoo.com>
2022-05-07 12:00:40 -0400

    banner adjustment to contain buttons

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,37 +2,48 @@ import React, { useEffect } from 'react';
 import anime from 'animejs/lib/anime.es.js';
 
 export default function Banner() {
-    useEffect(() => {
-        anime.timeline()
-        .add({
-          targets: '.banner__intro__text',
-          opacity: 0,
-          duration: 200,
-          easing: "easeOutExpo",
-        })
-        .add({
-            targets: '.banner__intro__text',
-            easing: "easeOutExpo",
-            rotateY: [-90, 0],
-            duration: 1300,
-            opacity: 1,
-            delay: 500
-        })
-        .add({
-            targets: '.banner__intro__subtext',
-            opacity: 0,
-            duration: 200,
-            easing: "easeOutExpo",
-          })
-          .add({
-              targets: '.banner__intro__subtext',
-              easing: "easeOutExpo",
-              rotateY: [-90, 0],
-              duration: 1300,
-              opacity: 1,
-              delay: 500
-          });
-    }, []);
+    // useEffect(() => {
+    //     anime.timeline()
+    //     .add({
+    //       targets: '.banner__intro__text',
+    //       opacity: 0,
+    //       duration: 200,
+    //       easing: "easeOutExpo",
+    //     })
+    //     .add({
+    //         targets: '.banner__intro__text',
+    //         easing: "easeOutExpo",
+    //         rotateY: [-90, 0],
+    //         duration: 500,
+    //         opacity: 1
+    //     })
+    //     .add({
+    //         targets: '.banner__intro__subtext',
+    //         opacity: 0,
+    //         duration: 200,
+    //         easing: "easeOutExpo",
+    //       })
+    //       .add({
+    //         targets: '.banner__intro__subtext',
+    //         easing: "easeOutExpo",
+    //         rotateY: [-90, 0],
+    //         opacity: 1,
+    //         delay: 200
+    //       })
+    //       .add({
+    //         targets: '.banner__intro__btn-group',
+    //         easing: 'easeOutExpo',
+    //         opacity: 0,
+    //         duration: 500
+    //       })
+    //       .add({
+    //         targets: '.banner__intro__btn-group',
+    //         easing: 'easeOutExpo',
+    //         rotateX: [-90, 0],
+    //         opacity: 1,
+    //         duration: 500
+    //       });
+    // }, []);
 
     return (
         <div className="banner">
@@ -43,6 +54,10 @@ export default function Banner() {
                     </div>
                     <div className='col-12'>
                         <div className='banner__intro__subtext'>Ask a way</div>
+                        <div className='banner__intro__btn-group'>
+                            <a class="cbtn cbtn--primary" href="#" role="button">RANDOM</a>
+                            <a class="cbtn cbtn--secondary" href="#" role="button">SEARCH</a>
+                        </div>
                     </div>
                 </div>
             </div>
